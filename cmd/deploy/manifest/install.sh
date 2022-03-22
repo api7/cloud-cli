9135e24a8a12dfb9e2d932116d475a35250d184a
Daming <zteny@foxmail.com>
2022-03-22 17:23:43 +0800

    feat: show instance id when it deployed successfully on bare metal (#25)

--- cmd/deploy/manifest/install.sh
+++ cmd/deploy/manifest/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 version="{{ .Version }}"
+instance_id="{{ .InstanceID }}"
 
 installed_version=$(apisix version 2>/dev/null)
 if [[ $? -ne 0 ]]; then
@@ -11,4 +14,14 @@ fi
 # copy certs to apisix directory to avoid permission issue
 cp -rf {{ .TLSDir }} /usr/local/apisix/conf/ssl
 
+# get the APISIX instance id when instance id is not set
+if [[ -z ${instance_id} ]]; then
+  instance_id="$(cat /usr/local/apisix/conf/apisix.uid)"
+fi
+
 apisix start -c {{ .ConfigFile }}
+
+if [[ $? -eq 0 ]]; then
+  echo "Your APISIX Instance was deployed successfully!"
+  echo "Instance ID: ${instance_id}"
+fi
