1e7acd169d5ebe0b4c83f28f2268af9f34b636d4
Daming <zteny@foxmail.com>
2022-03-28 09:09:46 +0800

    feat: support to customize apisix instance id on bare metal (#32)

--- cmd/deploy/manifest/install.sh
+++ cmd/deploy/manifest/install.sh
@@ -4,24 +4,33 @@ set -e
 
 version="{{ .Version }}"
 instance_id="{{ .InstanceID }}"
+apisix_home="/usr/local/apisix"
 
-installed_version=$(apisix version 2>/dev/null)
-if [[ $? -ne 0 ]]; then
+installed_version=$(apisix version 2>/dev/null) || true
+if [[ -z ${installed_version} ]]; then
   yum install -y {{ .APISIXRepoURL }}
   yum install -y apisix-$version
 fi
 
 # copy certs to apisix directory to avoid permission issue
-cp -rf {{ .TLSDir }} /usr/local/apisix/conf/ssl
+cp -rf {{ .TLSDir }} ${apisix_home}/conf/ssl
 
-# get the APISIX instance id when instance id is not set
-if [[ -z ${instance_id} ]]; then
-  instance_id="$(cat /usr/local/apisix/conf/apisix.uid)"
+if [[ -n ${instance_id} ]]; then
+  echo "${instance_id}" > ${apisix_home}/conf/apisix.uid
 fi
 
 apisix start -c {{ .ConfigFile }}
+status=$?
 
-if [[ $? -eq 0 ]]; then
+# wait for APISIX started and generated instance id
+sleep 1
+
+# get the APISIX instance id when instance id is not set
+if [[ -z ${instance_id} ]]; then
+  instance_id="$(cat ${apisix_home}/conf/apisix.uid)"
+fi
+
+if [[ $status -eq 0 ]]; then
   echo "Your APISIX Instance was deployed successfully!"
   echo "Instance ID: ${instance_id}"
 fi
