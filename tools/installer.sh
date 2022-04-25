e7c9e22dcf9651b2ca35dfb460876226ccbc6888
tokers <zchao1995@gmail.com>
2022-04-25 15:04:31 +0800

    feat: add installer.sh
    Signed-off-by: tokers <zchao1995@gmail.com>

---
+++ tools/installer.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+#
+#  Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements.  See the NOTICE file distributed with
+#  this work for additional information regarding copyright ownership.
+#  The ASF licenses this file to You under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with
+#  the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+SUPPORTED_OS_LIST="darwin linux"
+SUPPORTED_ARCH_LIST="amd64 arm64"
+
+error() {
+  echo -e "\033[31m> $1\033[0m"
+  exit 1
+}
+
+validate_os() {
+  for os in $SUPPORTED_OS_LIST; do
+    if [[ "$os" == "$1" ]]; then
+      return 0
+    fi
+  done
+
+  error "Unsupported OS: $1"
+}
+
+validate_arch() {
+  for arch in $SUPPORTED_ARCH_LIST; do
+    if [[ "$arch" == "$1" ]]; then
+      return 0
+    fi
+  done
+
+  error "Unsupported Arch: $1"
+}
+
+install_cloud_cli() {
+  curl https://github.com/api7/cloud-cli/releases/download/${1}/cloud-cli-${2}-${3}-${1}.gz -sLo /tmp/cloud-cli.gz
+  gzip -d -f /tmp/cloud-cli.gz
+  chmod a+x /tmp/cloud-cli
+  echo "Cloud CLI was installed successfully in /tmp/cloud-cli"
+}
+
+OS=`uname -s | tr A-Z a-z`
+ARCH=`uname -m | tr A-Z a-z`
+CLOUD_CLI_VER="0.8.0"
+
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH=amd64
+fi
+
+validate_arch $ARCH
+validate_os $OS
+install_cloud_cli $CLOUD_CLI_VER $OS $ARCH
